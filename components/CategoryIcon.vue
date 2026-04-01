1278ae15271700baa488392ccc15e325d42f1701
Chris Andrikanich <chris@andrikanich.com>
2026-04-01 16:37:09 -0400

    feat: add 8 category SVG icon components and CategoryIcon wrapper

---
+++ components/CategoryIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ slug: string }>()
+</script>
+
+<template>
+  <IconEligibility  v-if="slug === 'eligibility'" />
+  <IconTimelines    v-else-if="slug === 'timelines'" />
+  <IconOutreach     v-else-if="slug === 'outreach'" />
+  <IconShowcases    v-else-if="slug === 'showcases'" />
+  <IconScholarships v-else-if="slug === 'scholarships'" />
+  <IconDivisions    v-else-if="slug === 'divisions'" />
+  <IconCommitments  v-else-if="slug === 'commitments'" />
+  <IconNcaaRules    v-else-if="slug === 'ncaa-rules'" />
+</template>
